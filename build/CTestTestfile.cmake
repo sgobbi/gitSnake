93c16b2dbab38ffe1daee80b68db7d3bc011926c
Salome Gobbi <salomegobbi@Salomes-MacBook-Pro.local>
2020-12-16 14:05:30 +0100

    comp

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/salomegobbi/info/cplusplus/gitSnake
+# Build directory: /Users/salomegobbi/info/cplusplus/gitSnake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
